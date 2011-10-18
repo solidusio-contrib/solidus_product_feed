a5bf523a8c83082db049202a9e8b9fd54a926108
Josh Nussbaum <joshnuss@gmail.com>
2011-10-17 20:13:52 -0400

    added a more descriptive title to the rss link

--- app/overrides/rss_link.rb
+++ app/overrides/rss_link.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => 'layouts/spree_application',
                      :name => 'product_rss_link',
                      :insert_bottom => "[data-hook='inside_head']",
-                     :text => "<%= auto_discovery_link_tag(:rss, products_path(:format => :rss)) %>")
+                     :text => '<%= auto_discovery_link_tag(:rss, products_path(:format => :rss), {:title => "#{Spree::Config[:site_title]} Products"}) %>')
