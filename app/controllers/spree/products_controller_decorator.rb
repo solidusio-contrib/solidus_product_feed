2e02bc5a43b61fbf4ad17827436c6719f9cb6996
Steven Barragan <steven.barragan@crowdint.com>
2013-10-29 12:20:13 -0600

    Ensure to get all products on rss * Add a before filter to get all products  if request es rss

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -1,5 +1,15 @@
 Spree::ProductsController.class_eval do
   respond_to :rss, :only => [:index]
+  before_filter :check_rss, only: :index
+
+  def check_rss
+    if request.format.rss?
+      @products = Spree::Product.all
+      respond_to do |format|
+        format.rss
+      end
+    end
+  end
 
   caches_page :index, :if => Proc.new {|c| c.request.format.rss? }
 end
