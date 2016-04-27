5993006ef0422889864c2cf998176bfe245bb22e
Kevin Attfield <kevin@stembolt.com>
2016-04-27 10:46:18 -0700

    Fix spec helper support file loader
    This line would work in a normal app, but the specs are run in the
    context of the dummy app, so Rails.root + spec is not going to find
    anything.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,9 +5,7 @@ require 'rspec/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
-require 'spree/core/testing_support/factories'
+Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |file| require file }
 
 RSpec.configure do |config|
   # == Mock Framework
