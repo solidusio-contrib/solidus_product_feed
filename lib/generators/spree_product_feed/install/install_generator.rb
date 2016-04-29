6a949c74ee2fc50ff953b77a505cf41f065d25fd
Kevin Attfield <kevin@stembolt.com>
2016-04-29 12:44:10 -0700

    fix install generator

--- lib/generators/spree_product_feed/install/install_generator.rb
+++ lib/generators/spree_product_feed/install/install_generator.rb
@@ -1,15 +1,20 @@
 module SpreeProductFeed
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      class_option :auto_run_migrations, :type => :boolean, :default => false
 
       def add_migrations
-        run 'bundle exec rake railties:install:migrations FROM=spree_product_feed'
+        run 'rake railties:install:migrations FROM=spree_product_feed'
+      end
+
+      def add_javascripts
+        append_file "vendor/assets/javascripts/spree/backend/all.js", "//= require spree/backend/spree_product_feed\n"
       end
 
       def run_migrations
-         res = ask "Would you like to run the migrations now? [Y/n]"
-         if res == "" || res.downcase == "y"
-           run 'bundle exec rake db:migrate'
+        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
+        if run_migrations
+           run 'rake db:migrate'
          else
            puts "Skiping rake db:migrate, don't forget to run it!"
          end
