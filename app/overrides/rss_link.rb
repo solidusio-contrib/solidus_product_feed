6fd89e303f9b691c9ccfc44f70368bce093380e2
Jared Norman <jared@creepywizard.com>
2016-05-09 11:35:10 -0700

    Add rubocop config

--- app/overrides/rss_link.rb
+++ app/overrides/rss_link.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(:virtual_path => 'layouts/spree_application',
-                     :name => 'product_rss_link',
-                     :original => '86987c7feaaea3181df195ca520571d801bbbaf3',
-                     :insert_bottom => "[data-hook='inside_head']",
-                     :text => '<%= auto_discovery_link_tag(:rss, products_path(:format => :rss), {:title => "#{Spree::Config[:site_title]} Products"}) %>')
+Deface::Override.new(virtual_path: 'layouts/spree_application',
+                     name: 'product_rss_link',
+                     original: '86987c7feaaea3181df195ca520571d801bbbaf3',
+                     insert_bottom: "[data-hook='inside_head']",
+                     text: '<%= auto_discovery_link_tag(:rss, products_path(:format => :rss), {:title => "#{Spree::Config[:site_title]} Products"}) %>')
