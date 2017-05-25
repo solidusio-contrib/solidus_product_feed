6d6f6d90e493db97a48ae961f989ce115b7494d0
John Hawthorn <john@stembolt.com>
2017-05-25 04:57:41 -0700

    WIP

--- app/overrides/rss_link.rb
+++ app/overrides/rss_link.rb
@@ -2,4 +2,4 @@ Deface::Override.new(virtual_path: 'layouts/spree_application',
                      name: 'product_rss_link',
                      original: '86987c7feaaea3181df195ca520571d801bbbaf3',
                      insert_bottom: "[data-hook='inside_head']",
-                     text: '<%= auto_discovery_link_tag(:rss, products_path(:format => :rss), {:title => "#{Spree::Config[:site_title]} Products"}) %>')
+                     partial: 'solidus_product_feed/head')
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Spree::Core::Engine.routes.draw do
-  get '/product_feed', as: :product_feed, format: :rss
+  get '/product_feed', to: 'solidus_product_feed/products#index', as: :product_feed, format: :rss
 end
